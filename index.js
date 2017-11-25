c20bdc8ee7c8a8a6df9658429ce176c237945d8d
patrickeddy <p@patrickeddy.com>
2017-11-25 00:31:05 -0800

    Some fancy new state management + cleaner dom updates.

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 let dogButton,
     dogButton2,
     autoFetchButton,
-    autoFetchingText,
+    autoFetchTools,
+    autoFetchText,
     autoFetchIntervalInput,
     divDogs
     = null
 
 // Utils
+const el = (id) => window.document.getElementById(id)
 const API_URL = 'https://dog.ceo/api/breeds/image/'
 // Generic compose function (Who needs lodash, am I right?)
-const compose = (...fns) => (...args) => fns.reduceRight((acc, fn) => fn(acc))
+const compose = (...fns) => fns.reduceRight((g, f) => (...args) => f(g(...args)))
+const trace = (message) => f => {
+  console.log(message,f)
+  return f
+}
 // Fetch the dog data
 const fetchDogImage = (breed = 'random') => fetch(API_URL + breed)
 
@@ -19,6 +25,11 @@ const _state = {
     autoFetch: {
       on: false,
       freq: 1000
+    },
+    dogs: {
+      oldSize: 0,
+      size: 0,
+      all: []
     }
 }
 const stateValidator = {
@@ -37,26 +48,47 @@ Object.keys(_state).map(key => state[key] = new Proxy(_state[key], stateValidato
 const makeStateBroadcaster = (...fns) => () => fns.map(fn => fn())
 // stateChanged is a onchange broadcaster to State Change Handlers (SCH)
 const stateChanged = makeStateBroadcaster(
-  SCHAutoFetch
+  SCHDogs,
+  SCHAutoFetch,
+  trace('~~ App State Changed ~~')
 )
 
+// StateChangeHandler - Dogs
+function SCHDogs() {
+  if (state.dogs.oldSize < state.dogs.size){
+    // Add the missing dogs to the document
+    const dogDif = state.dogs.all.slice(state.dogs.oldSize)
+    dogDif.map(d => d.xf(dogsDiv))
+    state.dogs.oldSize = state.dogs.size
+  }
+}
+
 // StateChangeHandler - AutoFetch
-function SCHAutoFetch(){
+function SCHAutoFetch() {
   if (state.autoFetch.on){
-    autoFetchingText.style.cssText = 'display: inline-block;'
-    autoFetchIntervalInput.style.cssText = 'display: inline-block;'
+    autoFetchTools.classList.add("on")
+    autoFetchButton.innerHTML = 'Auto fetch = on'
   } else {
-    autoFetchingText.style.cssText = 'display: none;'
-    autoFetchIntervalInput.style.cssText = 'display: none;'
+    autoFetchTools.classList.remove("on")
+    autoFetchButton.innerHTML = 'Auto fetch = off'
   }
   const newFreq = parseInt(autoFetchIntervalInput.value)
   if (state.autoFetch.freq !== newFreq){
     state.autoFetch.freq = newFreq
   }
-  autoFetchingText.innerHTML = `Auto fetching (${newFreq/1000}s)`
+  autoFetchText.innerHTML = `<h2>Every ${ Math.floor(((newFreq/1000) * 100))/100 } seconds!</h2>`
 }
 
 // Helpers
+const Dogs = {
+  clear () {
+    dogsDiv.innerHTML = ""
+    state.dogs.all.length = 0
+    state.dogs.size = 0
+    state.dogs.oldSize = 0
+  }
+}
+
 const AutoFetch = {
   start (/* fetch function */f) {
     state.autoFetch.on = true
@@ -72,37 +104,55 @@ const AutoFetch = {
   _loop (f) {
     f()
     if (state.autoFetch.on) {
-      setTimeout(() => this._loop(f), state.autoFetch.freq)
+      setTimeout(this._loop.bind(this, f), state.autoFetch.freq)
     }
   }
 }
 
-// xfs
-const imgHtml = url => "<img width='350' src='" + url + "' />"
-const prependDog = (el, url) => el.innerHTML = `<div>${imgHtml(url) + el.innerHTML}</div>`
-const appendDog = (el, url) => el.innerHTML = `<div>${el.innerHTML + imgHtml(url)}</div>`
+// dog html xfs
+const d = dog => `
+    <div class="dog">
+        <h1>Doggie #${ dog.id }</h1>
+        <img src="${ dog.url }" title="${ dog.id }" />
+    </div>
+`
+const prependDogHtmlXf = id => el => el.innerHTML = `${ d(state.dogs.all[id]) + el.innerHTML }`
+const appendDogHtmlXf = id => el => el.innerHTML = `${ el.innerHTML + d(state.dogs.all[id]) }`
 
-// partial functions
-const makeAddDogImg = addXf => el => url => addXf(el, url)
-const prependAddDog = makeAddDogImg(prependDog)
-const appendAddDog = makeAddDogImg(appendDog)
-const fetchAndSetDog = (addXf) => () => {
+// add the dog to state
+const makeAddDog = addXf => dogUrl => {
+  const newId = state.dogs.all.length
+  const newDog = { id: newId, url: dogUrl, xf: addXf(newId) }
+  state.dogs.all.push( newDog )
+  state.dogs.size += 1
+}
+
+// ready-for-use partials
+const prependDog = makeAddDog(prependDogHtmlXf)
+const appendDog = makeAddDog(appendDogHtmlXf)
+
+// fetch dog
+const fetchDogUrl = (xfCallback) => {
   fetchDogImage()
     .then(response => response.json())
     .then(dog => dog.message)
-    .then(addXf)
+    .then(xfCallback)
 }
+const composeFetch = (xfPartial) => fetchDogUrl.bind(null, xfPartial)
 
 // Run app
 function run() {
   const body = `
   <div>
-    <div>
-      <button id="button-new-dog" style="font-size: 16px;">Get new dog</button>
-      <button id="button-new-dog-2" style="font-size: 16px;">Append new dog</button>
-      <button id="button-auto-fetch" style="font-size: 16px;">Toggle auto fetch</button>
-      <span id="text-auto-fetching" style="display:none;"></span>
-      <input id="range-auto-fetch-interval" style="display:none;" type="range" min="500" max="3000" />
+    <div class="buttons">
+      <button id="button-new-dog">Get new dog</button>
+      <button id="button-new-dog-2">Append new dog</button>
+      <button id="button-auto-fetch"></button>
+      <button id="button-clear">Clear</button>
+      <div id="auto-fetch-tools">
+        <p id="text-auto-fetch"></p>
+        <p><input id="range-auto-fetch-interval" type="range" min="500" max="3000" /></p>
+      </div>
     </div>
     <div id="dogs" style="text-align:center"></div>
   </div>
@@ -111,22 +161,29 @@ function run() {
   document.body.innerHTML = body
 
   // Elements from body
-  dogButton = window.document.getElementById('button-new-dog')
-  dogButton2 = window.document.getElementById('button-new-dog-2')
-  autoFetchButton = window.document.getElementById('button-auto-fetch')
-  autoFetchIntervalInput = window.document.getElementById('range-auto-fetch-interval')
-  autoFetchingText = window.document.getElementById('text-auto-fetching')
-  divDogs = window.document.getElementById('dogs')
+  dogButton = el('button-new-dog')
+  dogButton2 = el('button-new-dog-2')
+  autoFetchButton = el('button-auto-fetch')
+  autoFetchTools= el('auto-fetch-tools')
+  autoFetchText = el('text-auto-fetch')
+  autoFetchIntervalInput = el('range-auto-fetch-interval')
+  clearButton = el('button-clear')
+  dogsDiv = el('dogs')
 
   // event listeners
-  dogButton.onclick = compose(fetchAndSetDog, prependAddDog, divDogs)
-  dogButton2.onclick = compose(fetchAndSetDog, appendAddDog, divDogs)
-  autoFetchButton.onclick = compose(AutoFetch.toggle, fetchAndSetDog, prependAddDog, divDogs)
-  autoFetchIntervalInput.onchange = stateChanged
+  const prependHandler = composeFetch(prependDog)
+  const appendHandler = composeFetch(appendDog)
+  const toggleHandler = composeFetch(prependDog)
+  const clearHandler = Dogs.clear
 
+  dogButton.onclick = prependHandler
+  dogButton2.onclick = appendHandler
+  autoFetchButton.onclick = () => AutoFetch.toggle(toggleHandler)
+  autoFetchIntervalInput.onchange = stateChanged
+  clearButton.onclick = clearHandler
 
   // default call
-  fetchAndSetDog(prependAddDog(divDogs))()
+  fetchDogUrl(prependDog)
 }
 
 window.onload = run
